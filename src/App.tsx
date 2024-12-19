0e91fec02fa9545a27caee80679a20db295ea439
Yunfan Yang <cloudy.young@outlook.com>
2024-12-19 04:34:44 -0700

    Fix colors

--- src/App.tsx
+++ src/App.tsx
@@ -99,10 +99,10 @@ export const Slot = ({ isQueen, isCrossed, isConflicted, region, disabled, onCli
       onClick={onClick}
     >
       <span className={clsx(
-        'material-symbols-sharp',
+        'material-symbols-sharp text-zinc-900',
         (!isQueen && isCrossed) && 'text-zinc-600',
-        (isQueen && isCrossed) && 'text-red-600',
-        (isQueen && isConflicted) && 'text-red-600',
+        (isQueen && isCrossed) && 'text-red-600 dark:text-red-700',
+        (isQueen && isConflicted) && 'text-red-600 dark:text-red-700',
       )}>
         {icon}
       </span>
@@ -111,24 +111,24 @@ export const Slot = ({ isQueen, isCrossed, isConflicted, region, disabled, onCli
 }
 
 const REGION_COLORS = [
-  "bg-zinc-50",
-  "bg-red-100",
-  "bg-orange-100",
-  "bg-amber-100",
-  // "bg-yellow-100",
-  "bg-lime-100",
-  // "bg-green-100",
-  "bg-emerald-100",
-  "bg-teal-100",
-  "bg-cyan-100",
-  "bg-sky-100",
-  // "bg-blue-100",
-  "bg-indigo-100",
-  "bg-violet-100",
-  // "bg-purple-100",
-  "bg-fuchsia-100",
-  "bg-pink-100",
-  "bg-rose-100",
+  "bg-zinc-50 dark:bg-zinc-300",
+  "bg-red-100 dark:bg-red-300",
+  "bg-orange-100 dark:bg-orange-300",
+  "bg-amber-100 dark:bg-amber-300",
+  // "bg-yellow-100 dark:bg-yellow-300",
+  "bg-lime-100 dark:bg-lime-300",
+  // "bg-green-100 dark:bg-green-300",
+  "bg-emerald-100 dark:bg-emerald-300",
+  "bg-teal-100 dark:bg-teal-300",
+  "bg-cyan-100 dark:bg-cyan-300",
+  "bg-sky-100 dark:bg-sky-300",
+  // "bg-blue-100 dark:bg-blue-300",
+  "bg-indigo-100 dark:bg-indigo-300",
+  "bg-violet-100 dark:bg-violet-300",
+  // "bg-purple-100 dark:bg-purple-300",
+  "bg-fuchsia-100 dark:bg-fuchsia-300",
+  "bg-pink-100 dark:bg-pink-300",
+  "bg-rose-100 dark:bg-rose-300",
 ]
 
 const toggleCrossed = (slots: Slot[], index: number, isCrossed: boolean) => {
