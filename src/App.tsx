b7d19d3df01a0a7872d54681d4d11381d3e5105f
Yunfan Yang <cloudy.young@outlook.com>
2024-12-20 18:12:52 -0700

    Fix error

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ export const App = () => {
     <>
       <div className="max-w-2xl mx-auto text-zinc-950 dark:text-white p-2 sm:p-6">
         <div className="h-fit w-full space-y-6 py-10">
-          <div className="text-4xl font-extrabold text-center uppercase">Queens</div>
+          <div className="text-4xl font-extrabold text-center uppercase">// Queens //</div>
 
           <div className="bg-white shadow-[0px_0px_0px_1px_rgba(9,9,11,0.07),0px_2px_2px_0px_rgba(9,9,11,0.05)] dark:bg-zinc-900 dark:shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] dark:before:pointer-events-none dark:before:absolute dark:before:-inset-px dark:before:rounded-xl dark:before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
             <div className="p-2 sm:p-4">
@@ -67,8 +67,16 @@ export const App = () => {
             <Button onClick={onNewGame}>New Game</Button>
           </div>
 
-          <svg xmlns="http://www.w3.org/2000/svg" className="row-start-3 ml-[calc(50%-50vw)] h-px w-screen" fill="none"><defs><pattern id=":S2:" patternUnits="userSpaceOnUse" width="16" height="1"><line class="stroke-zinc-950 dark:stroke-white" x1="0" x2="16" y1="0.5" y2="0.5" stroke-dasharray="2 2" stroke-width="1.5" stroke-opacity="0.1" stroke-linejoin="round"></line></pattern></defs><rect width="100%" height="100%" fill="url(#:S2:)"></rect></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="row-start-3 ml-[calc(50%-50vw)] h-px w-screen" fill="none"><defs><pattern id=":S2:" patternUnits="userSpaceOnUse" width="16" height="1"><line className="stroke-zinc-950 dark:stroke-white" x1="0" x2="16" y1="0.5" y2="0.5" stroke-dasharray="2 2" stroke-width="1.5" stroke-opacity="0.1" stroke-linejoin="round"></line></pattern></defs><rect width="100%" height="100%" fill="url(#:S2:)"></rect></svg>
 
+          <div className="text-center">
+            <div className="text-xl font-medium italic">How to play</div>
+
+            <div>Each row can only have one queen.</div>
+            <div>Each column can only have one queen.</div>
+            <div>Each color region can only have one queen.</div>
+            <div>Two queens cannot touch each other, not even diagonally.</div>
+          </div>
         </div>
       </div>
     </>
