083dc08ff915ea81ad8e8174c30239692ac2bc66
Yunfan Yang <cloudy.young@outlook.com>
2025-01-02 00:59:15 -0700

    Add paddings

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -52,7 +52,7 @@ export const Play = () => {
 
   return (
     <>
-      <div className="max-w-2xl mx-auto text-zinc-950 dark:text-white">
+      <div className="max-w-2xl mx-auto text-zinc-950 dark:text-white py-4 md:py-10">
         <div className="h-fit w-full space-y-6">
 
           <div className="bg-white shadow-[0px_0px_0px_1px_rgba(9,9,11,0.07),0px_2px_2px_0px_rgba(9,9,11,0.05)] dark:bg-zinc-900 dark:shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] dark:before:pointer-events-none dark:before:absolute dark:before:-inset-px dark:before:rounded-xl dark:before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
