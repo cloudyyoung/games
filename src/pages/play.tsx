b9979aaa1a85dae9a9703f73d3b864fb8752fd57
Yunfan Yang <cloudy.young@outlook.com>
2025-01-02 04:10:20 -0700

    Update play.tsx

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -70,7 +70,7 @@ export const Play = () => {
 
         <div className="bg-white shadow-[0px_0px_0px_1px_rgba(9,9,11,0.07),0px_2px_2px_0px_rgba(9,9,11,0.05)] dark:bg-zinc-900 dark:shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] dark:before:pointer-events-none dark:before:absolute dark:before:-inset-px dark:before:rounded-xl dark:before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
           <div className="p-0 sm:p-4">
-            <div className="outline outline-[3px] outline-zinc-800 dark:outline-zinc-600">
+            <div className="outline outline-[3px] outline-zinc-800 dark:outline-zinc-600 m-0.5">
               <div className={`grid gap-0`} style={{ gridTemplateColumns: `repeat(${SIZE_N}, minmax(0, 1fr)` }}>
                 {
                   slots.map((slot) => (
