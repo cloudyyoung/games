6085e9b5901ae2fd8b8b02dfc8f483fd1c8e63fe
Yunfan Yang <cloudy.young@outlook.com>
2025-01-02 00:28:29 -0700

    Add routes

---
+++ src/components/app/navbar.tsx
@@ -0,0 +1,14 @@
+import { Navbar, NavbarItem, NavbarLabel, NavbarSection, NavbarSpacer, NavbarDivider } from "../../components/navbar"
+
+export const AppNavbar = () => {
+  return (
+    <Navbar>
+      <NavbarLabel className="uppercase font-extrabold text-xl">Queens</NavbarLabel>
+      <NavbarDivider className="max-lg:hidden" />
+      <NavbarSection className="max-lg:hidden">
+        <NavbarItem href='/'>Play</NavbarItem>
+      </NavbarSection>
+      <NavbarSpacer />
+    </Navbar>
+  )
+}
---
+++ src/components/app/sidebar.tsx
@@ -0,0 +1,16 @@
+import { Sidebar, SidebarBody, SidebarHeader, SidebarItem, SidebarSection } from "../../components/sidebar"
+
+export const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <div className="uppercase font-extrabold text-xl px-2 pt-4">Queens</div>
+      </SidebarHeader>
+      <SidebarBody>
+        <SidebarSection>
+          <SidebarItem href='/'>Play</SidebarItem>
+        </SidebarSection>
+      </SidebarBody>
+    </Sidebar>
+  )
+}
