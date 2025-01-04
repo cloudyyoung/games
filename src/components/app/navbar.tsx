245c997bf37ca580659367cb385145e17f4e3508
Yunfan Yang <cloudy.young@outlook.com>
2025-01-04 02:53:00 -0700

    Update navbar.tsx

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -1,9 +1,16 @@
 import { Navbar, NavbarItem, NavbarLabel, NavbarSection, NavbarSpacer, NavbarDivider } from "../../components/navbar"
+import { Dropdown, DropdownButton, DropdownDivider, DropdownItem, DropdownLabel, DropdownMenu } from "../dropdown"
 
 export const AppNavbar = () => {
   return (
     <Navbar>
-      <NavbarLabel className="font-extrabold text-xl">Queens</NavbarLabel>
+      <Dropdown>
+        <DropdownButton as={NavbarItem} className="max-lg:hidden">
+          <NavbarLabel className="font-extrabold text-xl">Queens</NavbarLabel>
+          <span className="material-symbols-sharp text-lg -ml-1 font-extrabold">expand_all</span>
+        </DropdownButton>
+        <GamesDropdownMenu />
+      </Dropdown>
       <NavbarDivider className="max-lg:hidden" />
       <NavbarSection className="max-lg:hidden">
         <NavbarItem href=''>Play</NavbarItem>
@@ -11,5 +18,19 @@ export const AppNavbar = () => {
       </NavbarSection>
       <NavbarSpacer />
     </Navbar>
+  )
+}
+
+export const GamesDropdownMenu = () => {
+  return (
+    <DropdownMenu className="min-w-80 lg:min-w-64" anchor="bottom start">
+      <DropdownItem href="/">
+        <DropdownLabel>Home</DropdownLabel>
+      </DropdownItem>
+      <DropdownDivider />
+      <DropdownItem href="/queens">
+        <DropdownLabel>Queens</DropdownLabel>
+      </DropdownItem>
+    </DropdownMenu>
   )
 }
